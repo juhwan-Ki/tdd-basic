0b6f61f49234fcbff4077ee645df72929dae8ad3
juhwan <kjhw990218@gmail.com>
2025-08-15 17:56:10 +0900

    feat: 포인트 조회 기능 및 테스트 코드 추가 - 포인트 조회 기능 추가 및 테스트 코드 추가 - 해당 기능 추가 하면서 PointService에 검증 로직을 메소드로 추출하여 리팩토링함 - 예외를 세분화 하여 예외 발생시 어떤 예외가 발생 하였는지 알기 쉽게 처리 - 로그를 활용하여 성공/롤백/실패에 대한 내용을 로깅하도록 함

--- src/test/java/io/hhplus/tdd/point/service/PointService.java
+++ src/test/java/io/hhplus/tdd/point/service/PointService.java
@@ -2,10 +2,10 @@ package io.hhplus.tdd.point.service;
 
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.PointPolicy;
 import io.hhplus.tdd.point.TransactionType;
 import io.hhplus.tdd.point.UserPoint;
-import io.hhplus.tdd.point.exception.PointSaveException;
-import io.hhplus.tdd.point.exception.PointValidationException;
+import io.hhplus.tdd.point.exception.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,39 +16,21 @@ public class PointService {
     private final UserPointTable pointTable;
     private final PointHistoryTable pointHistoryTable;
 
-    private final static Logger logger = LoggerFactory.getLogger(PointService.class);
+    private static final Logger logger = LoggerFactory.getLogger(PointService.class);
     
     public PointService(UserPointTable pointTable, PointHistoryTable pointHistoryTable) {
         this.pointTable = pointTable;
         this.pointHistoryTable = pointHistoryTable;
     }
 
-    public long getBalance(Long userId) {
-        UserPoint userPoint = pointTable.selectById(userId);
-        // TODO: 현재는 무조건 null이 아닌 값이 나와서 해당 로직이 필요 없긴함..
-        if(userPoint == null)
-            return 0;
-        return userPoint.point();
-    }
-
     // 최소 충전 금액은 1000원 최대 충전 금액은 100만원으로 한다
     public UserPoint charge(Long userId, long chargeAmount) {
-        if(userId == null || userId <= 0)
-            throw new IllegalArgumentException("잘못된 값이 입력되었습니다. userId : " + userId);
-
-        if(chargeAmount < MIN_AMOUNT.value())
-            throw new PointValidationException("충전 금액은 " + MIN_AMOUNT + "원 보다 커야 합니다");
-
-        if(chargeAmount > MAX_AMOUNT.value())
-            throw new PointValidationException("충전 금액은 " + MAX_AMOUNT + "원 보다 클 수 없습니다");
-
-        if(chargeAmount % POINT_CHARGE_UNIT.value() != 0)
-            throw new PointValidationException("충전 금액은 " + POINT_CHARGE_UNIT + "원 단위 여야 합니다");
+        validateUserId(userId);
+        validateAmount(chargeAmount, "충전 금액은 ", POINT_CHARGE_UNIT);
 
         UserPoint currentPoint = pointTable.selectById(userId);
         long updatedBalance = currentPoint.point() + chargeAmount;
-        if(updatedBalance > MAX_POINT_BALANCE.value())
-            throw new PointValidationException("최대 보유 가능 포인트를 초과할 수 없습니다. 최대 보유 가능 포인트 : "  + MAX_POINT_BALANCE);
+        validatePointBalance(updatedBalance);
 
         // 잔액 충전 중이나 이력 업데이트시 에러날 때 rollback
         try {
@@ -58,55 +40,92 @@ public class PointService {
                 pointHistoryTable.insert(userId, chargeAmount, TransactionType.CHARGE, System.currentTimeMillis());
             } catch (Exception e) {
                 rollback(currentPoint);
+                logger.error("포인트 충전 실패로 인한 롤백 userId={}, 잔액={}", userId, currentPoint.point(), e);
                 throw new PointSaveException("포인트 이력 저장 실패", e);
             }
+
+            logger.info("포인트 충전 완료 userId={}, 충전 포인트={} 잔액={}", userId, chargeAmount, updatedBalance);
             return updatedPoint;
         } catch (Exception e) {
+            logger.error("포인트 충전 실패 userId={}, 충전 금액={}", userId, chargeAmount, e);
             throw new PointSaveException("포인트 잔액 저장 실패", e);
         }
     }
 
     // 최소 사용 금액은 1000원 최대 사용 금액은 100만원으로 한다
     public UserPoint use(Long userId, long useAmount) {
-        if(userId == null || userId <= 0)
-            throw new IllegalArgumentException("잘못된 값이 입력되었습니다. userId : " + userId);
-
-        if(useAmount < MIN_AMOUNT.value())
-            throw new PointValidationException("사용 금액은 " + MIN_AMOUNT + "원 보다 커야 합니다");
-
-        if(useAmount > MAX_AMOUNT.value())
-            throw new PointValidationException("사용 금액은 " + MAX_AMOUNT + "원 보다 클 수 없습니다");
-
-        if(useAmount % POINT_USE_UNIT.value() != 0)
-            throw new PointValidationException("사용 금액은 " + POINT_USE_UNIT + "원 단위 여야 합니다");
+        validateUserId(userId);
+        validateAmount(useAmount, "사용 금액은 ", POINT_USE_UNIT);
 
         UserPoint currentPoint = pointTable.selectById(userId);
         long currentBalance = currentPoint.point();
         if(currentBalance <= 0)
             throw new PointValidationException("사용 가능한 포인트가 없습니다.");
 
-        long updatedAmount = currentBalance - useAmount;
-        if(updatedAmount < 0)
-            throw new PointValidationException("사용 가능한 포인트가 부족합니다. 사용 가능 포인트 : " + currentBalance);
+        long updatedBalance = currentBalance - useAmount;
+        validatePointBalance(updatedBalance);
 
-        try
-        {
-            UserPoint updatedPoint = pointTable.insertOrUpdate(userId, updatedAmount);
+        try {
+            UserPoint updatedPoint = pointTable.insertOrUpdate(userId, updatedBalance);
 
             try {
                 pointHistoryTable.insert(userId, useAmount, TransactionType.USE, System.currentTimeMillis());
             } catch (Exception e) {
                 rollback(currentPoint);
+                logger.error("포인트 사용 실패로 인한 롤백 userId={}, 잔액={}", userId, currentBalance, e);
                 throw new PointSaveException("포인트 이력 저장 실패", e);
             }
 
+            logger.info("포인트 사용 완료 userId={}, 사용 포인트={} 잔액={}", userId, useAmount, updatedBalance);
             return updatedPoint;
         } catch (Exception e) {
+            logger.error("포인트 사용 실패 userId={}, 사용 금액={}", userId, useAmount, e);
             throw new PointSaveException("포인트 사용 실패", e);
         }
     }
 
+    public UserPoint getBalance(Long userId) {
+        validateUserId(userId);
+
+        try {
+            UserPoint currentPoint = pointTable.selectById(userId);
+            validatePointBalance(currentPoint.point());
+            return currentPoint;
+        } catch (Exception e) {
+            logger.error("포인트 조회 실패 userId={}", userId, e);
+            throw new PointRetrieveException("포인트 조회 실패 ", e);
+        }
+    }
+
+    private static void validatePointBalance(long balance) {
+        if (balance > MAX_POINT_BALANCE.value())
+            throw new MaxPointBalanceExceededException(MAX_POINT_BALANCE.value(), balance);
+
+        if (balance < 0)
+            throw new NegativePointBalanceException(balance);
+    }
+
+    private static void validateAmount(long useAmount, String prefix, PointPolicy pointUseUnit) {
+        if (useAmount < MIN_AMOUNT.value())
+            throw new PointValidationException(prefix + MIN_AMOUNT.value() + "원 이상이어야 합니다");
+
+        if (useAmount > MAX_AMOUNT.value())
+            throw new PointValidationException(prefix + MAX_AMOUNT.value() + "원을 초과할 수 없습니다");
+
+        if (useAmount % pointUseUnit.value() != 0)
+            throw new PointValidationException(prefix + pointUseUnit + "원 단위 여야 합니다");
+    }
+
+    private static void validateUserId(Long userId) {
+        if(userId == null || userId <= 0 )
+            throw new IllegalArgumentException("잘못된 값이 입력되었습니다. userId : " + userId);
+    }
+
     private void rollback(UserPoint rollbackPoint) {
-        pointTable.insertOrUpdate(rollbackPoint.id(), rollbackPoint.point());
+        try {
+            pointTable.insertOrUpdate(rollbackPoint.id(), rollbackPoint.point());
+        } catch (Exception ex) {
+            logger.error("포인트 롤백 실패 userId={}, snapshot={}", rollbackPoint.id(), rollbackPoint.point(), ex);
+        }
     }
 }
